638337d0a5b2320200749fc538ce2747fc1a7d0a
lucky <lucky@lucky.ilbirs.com>
2017-09-25 17:13:05 +0300

    all done

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,17 @@
+import sys
+import os
+from collections import defaultdict
+
+def search_doubles(dirpath):
+    compare_files = defaultdict(list)
+    for (dirpath, dirnames, filenames) in os.walk(dirpath):
+        for fname in filenames:
+            fsize = os.path.getsize('%s/%s' % (dirpath, fname))
+            compare_files[(fname, fsize)].append(dirpath)
+    return filter(lambda x: len(x[1]) > 1 ,compare_files.items())
+
 
 if __name__ == '__main__':
-    pass
+    for found_file in search_doubles(sys.argv[1]):
+        for path in found_file[1]:
+            print('%d %s/%s' % (found_file[0][1], path, found_file[0][0]))
