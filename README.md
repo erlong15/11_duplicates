5d51b36149d2d2ea0acea7d655a27cbbbe3d4c17
Alex <lucky_is@mail.ru>
2017-10-05 01:51:04 +0300

    notices have been fixed

--- README.md
+++ README.md
@@ -6,13 +6,23 @@
 
 На вход подается путь к папке.
 
+```
+python3 duplicates.py -h
+usage: duplicates.py [-h] -p PATH
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PATH, --path PATH  start directory for searching duplicates
+
+```
+
 Скрипт выводит все найденные дубликаты в формате 
 *<размер файла> <полный путь к файлу>*
 
 Пример запуска
 
 ```
-[lucky@lucky 11_duplicates]$ python3 duplicates.py /home/lucky/devman/
+[lucky@lucky 11_duplicates]$ python3 duplicates.py -p /home/lucky/devman/
 11 /home/lucky/devman/4_json/.git/COMMIT_EDITMSG
 11 /home/lucky/devman/3_bars/.git/COMMIT_EDITMSG
 
--- duplicates.py
+++ duplicates.py
@@ -1,17 +1,28 @@
 import sys
 import os
+import argparse
 from collections import defaultdict
+
 
 def search_doubles(dirpath):
     compare_files = defaultdict(list)
-    for (dirpath, dirnames, filenames) in os.walk(dirpath):
+    for dirpath, dirnames, filenames in os.walk(dirpath):
         for fname in filenames:
-            fsize = os.path.getsize('%s/%s' % (dirpath, fname))
+            fsize = os.path.getsize(os.path.join(dirpath, fname))
             compare_files[(fname, fsize)].append(dirpath)
-    return filter(lambda x: len(x[1]) > 1 ,compare_files.items())
+    return filter(lambda x: len(x[1]) > 1, compare_files.items())
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path',
+                        help='start directory for searching duplicates',
+                        required=True)
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    for found_file in search_doubles(sys.argv[1]):
+    args = get_args()
+    for found_file in search_doubles(args.path):
         for path in found_file[1]:
             print('%d %s/%s' % (found_file[0][1], path, found_file[0][0]))
